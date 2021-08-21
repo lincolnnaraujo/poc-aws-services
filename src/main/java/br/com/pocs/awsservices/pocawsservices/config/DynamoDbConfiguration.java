cd7ea25f68ca5ef5bd9463f236598d3b95eeadd2
Lincoln Araujo <lincoln.n.araujo@gmail.com>
2021-08-21 16:36:42 -0300

    Add config - dynamodb

---
+++ src/main/java/br/com/pocs/awsservices/pocawsservices/config/DynamoDbConfiguration.java
@@ -0,0 +1,31 @@
+package br.com.pocs.awsservices.pocawsservices.config;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDbConfiguration {
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(buildAmazonDynamoDB());
+    }
+
+    private AmazonDynamoDB buildAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                "http://localhost:4566",
+                                Regions.EU_WEST_1.getName()
+                        )
+                )
+                .build();
+    }
+
+}
