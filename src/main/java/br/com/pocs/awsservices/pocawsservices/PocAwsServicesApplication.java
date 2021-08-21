0022fb94e34627c8b59635967149b3f17f5470eb
Lincoln Araujo <lincoln.n.araujo@gmail.com>
2021-08-21 16:05:02 -0300

    Initial commit

---
+++ src/main/java/br/com/pocs/awsservices/pocawsservices/PocAwsServicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.pocs.awsservices.pocawsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocAwsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocAwsServicesApplication.class, args);
+	}
+
+}
